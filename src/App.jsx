73cdb7898683d4a0f77a9b3e4a6a0e227e9b55ea
testrepo11244 <vivocaloid@gmail.com>
2026-07-19 22:01:43 +0700

    add src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,35 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import ProductList from './components/ProductList';
 import CartItem from './components/CartItem';
 import './App.css';
 
 function LandingPage() {
   const navigate = useNavigate();
+
   return (
-    <div className="landing">
-      <h1>Paradise Nursery</h1>
-      <button onClick={() => navigate('/products')}>Get Started</button>
+    <div className="landing-page">
+      <div className="landing-content">
+        <h1>Paradise Nursery</h1>
+        <p>Your neighborhood plant boutique</p>
+        <button className="get-started-btn" onClick={() => navigate('/products')}>
+          Get Started
+        </button>
+      </div>
     </div>
   );
 }
 
-function Navbar() {
-  const totalItems = useSelector(state => state.cart.items.reduce((sum, item) => sum + item.quantity, 0));
-  return (
-    <nav className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/products">Plants</Link>
-      <Link to="/cart">Cart ({totalItems})</Link>
-    </nav>
-  );
-}
-
-function Layout() {
-  return (
-    <div>
-      <Navbar />
-      <Outlet />
-    </div>
-  );
-}
-
-export default function App() {
+function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route element={<Layout />}>
-          <Route path="/products" element={<ProductList />} />
-          <Route path="/cart" element={<CartItem />} />
-        </Route>
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/cart" element={<CartItem />} />
       </Routes>
     </Router>
   );
 }
+
+export default App;
